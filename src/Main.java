a09db27269bae8603bf8c21b364b107a0410941f
k7dus <100138253@lubbockisd.net>
2022-09-13 09:53:36 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main
+{
+    public static void main ( String[] args )
+    {
+
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte one = 23;
+        char t = 97;
+        short rah = 94;
+        int blug = 24;
+        long uhh = 354253464L;
+        float flow = 5.4f;
+        boolean g = true;
+        String yes = "whats good gang";
+        double pulp = 3.14159265359;
+        System.out.println("Kidus                    9/13/22");
+        System.out.println("Byte" + "  " + one);
+        System.out.println("Char" + "  " + t);
+        System.out.println("long" + "  " + uhh);
+        System.out.println("Boolean" + "  " + g);
+        System.out.println("String" + "  " + yes);
+        System.out.println("Int" + "  " + blug);
+    }
+}
